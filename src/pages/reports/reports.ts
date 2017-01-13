11885e70b28a3dfabaf6811e8118b99fdae3e9b7
Michael Bates <michael.a.bates@gmail.com>
2017-01-13 12:50:27 -0600

    db communications

--- src/pages/reports/reports.ts
+++ src/pages/reports/reports.ts
@@ -1,12 +1,11 @@
-import { Component, OnInit        } from '@angular/core'  ;
+import { Component                } from '@angular/core'  ;
 import { NavController, NavParams } from 'ionic-angular'  ;
 import { FormBuilder, FormGroup   } from '@angular/forms' ;
 import { Validators, FormControl  } from '@angular/forms' ;
 import { Observable               } from "rxjs/Rx"        ;
-import * as moment                  from 'moment'         ;
 import { AuthTestUser } from '../../providers/auth-test-user';
 import { OSXU } from '../../config/user.config.user';
-
+import { DBSrvcs } from '../../providers/db-srvcs';
 
 @Component({
   selector: 'page-reports',
@@ -14,87 +13,27 @@ import { OSXU } from '../../config/user.config.user';
   styles: [ ".ng-invalid { background: rgba(255 150, 80, 0.5) }" ]
 })
 
-export class ReportsPage implements OnInit {
+export class ReportsPage {
 
-  // company = this.stngs.company;  
-  // loc1 = this.stngs.loc1;  
-  // loc2 = this.stngs.loc2; 
-  // techClass = this.stngs.techClass ;  
-  // technician = this.stngs.technician; 
-  // hrsClass = this.stngs.hrsClass;
+  items: any;
 
-  constructor( public navCtrl: NavController, public navParams: NavParams, ) { } // private _fb: FormBuilder, private _fc: FormControl, 
-// public stngs: SettingsPage
-   ngOnInit() {
-      // this.getTestUser();
-    }
+  constructor(public dbservcies: DBSrvcs){  }
 
-  
+  ionViewDidLoad(){
+    this.items = [];
+    this.dbservcies.getDocuments()
+      .then( (result) => { this.items = result; } );
+  }
+ 
+  addData(){
+    let date = new Date();
+    let docId = date.toDateString();
+    let newDoc = {
+      '_id': docId,
+      'message': date.getTime()
+    };
+
+    this.dbservcies.addDocument(newDoc);
+  }
+
 }
-  // workReport: FormGroup;
-
-  //  e   = this.workReport.value.end;
-  //  s   = this.workReport.value.start;
-  // en   : number;
-  // st   : number;
-  // valid: Boolean;
-
-// -------------------------------------------------
-    //  this.workReport = this._fb.group({
-    //    username  : [ ''   , Validators.required ] ,
-    //    today     : [ moment().format()          ] ,
-    //    uNum      : [ ''   , Validators.required ] ,
-    //    woNum     : [ ''                         ] ,
-    //    start     : [ '0:0', Validators.required ] ,
-    //    end       : [ '0:0', Validators.required ] ,
-    //    reportHrs : [ '0:0', Validators.required ] , // this.rh
-    //    woNotes   : [ ''   , Validators.required ]
-    //   })
-    // rh(control: FormControl) { }
-  // : Promise<any> {
-  //   const promise = new Promise<any>(
-  //     (resolve, reject) => {
-  //       if()
-  //     }
-  //   )
-  //   return promise;
-  // }
-  // ---------------------------------------------------
-  // esplit(e: any): number {
-  //   this.e.split(':'); 
-  //   if( e[1] <  8 ){ e[1] =  0; } else {
-  //   if( e[1] < 23 ){ e[1] = 15; } else {
-  //   if( e[1] < 38 ){ e[1] = 30; } else {
-  //   if( e[1] < 53 ){ e[1] = 45; } else
-  //                  { e[1] = 60; } } } }
-  //   this.en = (( +e[0] ) * 60  + ( +e[1] ))/60 || 0;
-
-  //   return this.en;
-  // }
-
-  // ssplit(s: any): number {
-  //   this.s.split(':');
-  //   if( s[1] <  8 ){ s[1] =  0; } else {
-  //   if( s[1] < 23 ){ s[1] = 15; } else {
-  //   if( s[1] < 38 ){ s[1] = 30; } else {
-  //   if( s[1] < 53 ){ s[1] = 45; } else
-  //                  { s[1] = 60; } } } }
-  //   this.st = (( +s[0]) * 60  + ( +s[1] ))/60 || 0;
-  //   return this.st;
-  // }
-  
-  // onSubmit(formData: any) {
-  //   console.log(formData);
-  // }
-
-  // procRprTime() {
-  //   this.esplit(this.e);
-  //   this.ssplit(this.s);
-  //   this.runNxt(this.st, this.en);
-  //   }
-
-  // runNxt(st, en) {
-  //   this.valid = false;
-  //   if(this.en < this.st) { this.en = this.en + 24; }
-
-  // }
--- src/providers/db-srvcs.ts
+++ src/providers/db-srvcs.ts
@@ -1,18 +1,73 @@
-import { Injectable } from '@angular/core';
+import { Injectable, NgZone } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
+import * as PouchDB from 'pouchdb';
 
-/*
-  Generated class for the DBSrvcs provider.
-
-  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
-  for more info on providers and Angular 2 DI.
-*/
 @Injectable()
 export class DBSrvcs {
 
-  constructor(public http: Http) {
-    console.log('Hello DBSrvcs Provider');
+  data    : any; 
+  db      : any; 
+  username: any; 
+  password: any; 
+  remote  : any;
+  
+
+  constructor(public http: Http, public zone: NgZone) {
+
+      this.db = new PouchDB('test_pouch-connections');
+      this.username = 'sesatech';
+      this.password = 'sesatech';
+      this.remote = 'http://martiancouch.hplx.net/test_pouch-connections';
+ 
+      let options = {
+        live      : true, 
+        retry     : true, 
+        continuous: true, 
+        auth      : { username: this.username, password: this.password }
+      };
+  
+      this.db.sync(this.remote, options);
+ 
+  }
+
+  addDocument(doc){ this.db.put(doc); }
+
+  getDocuments(){
+ 
+    return new Promise(resolve => {
+ 
+      this.db.allDocs({  include_docs: true })
+      .then((result) => { this.data = [];
+        let docs = result.rows.map((row) => {
+          this.data.push(row.doc);
+          resolve(this.data);
+        });
+ 
+        this.db.changes({live: true, since: 'now', include_docs: true}).on('change', (change) => {
+          this.handleChange(change);
+        });
+      }).catch((error) => {
+        console.log(error);
+      });
+    });
+  }
+
+  handleChange(change){
+ 
+    this.zone.run(() => { 
+      let changedDoc = null;
+      let changedIndex = null;
+ 
+    this.data.forEach((doc, index) => {
+      if(doc._id === change.id){ changedDoc = doc;  changedIndex = index; }
+      });
+
+    if(change.deleted){ this.data.splice(changedIndex, 1); } 
+      else { if(changedDoc){ this.data[changedIndex] = change.doc; } 
+      else { this.data.push(change.doc); } }
+      });
+
   }
 
 }
