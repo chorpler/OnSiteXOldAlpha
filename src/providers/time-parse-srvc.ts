8ebda208625b6074277de3c4cabae2c192841c3b
Michael Bates <michael.a.bates@gmail.com>
2017-05-19 05:41:03 -0500

    calc end hrs n sich

--- src/providers/time-parse-srvc.ts
+++ src/providers/time-parse-srvc.ts
@@ -26,6 +26,16 @@ export class TimeSrvc {
   gmtOffset   : any       ;
   tmZoneStr   : any       ;
   onSiteXDate : OSXD      ;
+  prsHrs;
+  strtHrs;
+  prsMin;
+  strtMin;
+  hrsHrs;
+  hrsMin;
+  endHrs;
+  timeEnds;
+  endMin;
+  
 
   constructor( ) { console.log('Hello TimeSrvc Provider'); }
 
@@ -47,6 +57,72 @@ export class TimeSrvc {
     this._arrD = [ this.month, this.weekDay, this.mDay, this.year, this.hours, this.min, this.sec, this.time, this.gmtOffset, this.tmZoneStr ];
     console.log(this._arrD);
     return this._arrD;
+  }
+
+  /**
+ * Calcualtes workOrderData.timeEnds given workOrderData.timeStarts
+ * and workOrderData.repairHrs
+ *
+ * @private
+ * @param {any} workOrderData
+ *
+ * @memberOf WorkOrder
+ */
+  private calcEndTime(workOrderData) {
+    const _Xdec = /(00|15|30|45)(?!\:\d{2})/;
+    const _Xhrs = /([0-9]{2})(?=:\d{2})/;
+
+    this.prsHrs = _Xhrs.exec(workOrderData.timeStarts);
+    this.strtHrs = parseInt(this.prsHrs[0]).toString();
+    this.prsMin = _Xdec.exec(workOrderData.timeStarts);
+
+    if (parseInt(this.prsMin[0]) === 0) {
+      this.strtMin = '00';
+    }
+    else if (parseInt(this.prsMin[0]) === 15) {
+      this.strtMin = '15';
+    }
+    else if (parseInt(this.prsMin[0]) === 30) {
+      this.strtMin = '30';
+    }
+    else {
+      this.strtMin = '45';
+    }
+
+    workOrderData.timeStarts = this.strtHrs + ':' + this.strtMin;
+
+    this.hrsHrs = Math.floor(workOrderData.repairHrs);
+    this.hrsMin = (workOrderData.repairHrs%1)*60;
+
+    if (parseInt(this.strtMin) + this.hrsMin > 60) {
+
+      if (parseInt(this.strtHrs) + this.hrsHrs + 1 > 24)
+            { this.endHrs = parseInt(this.strtHrs) + this.hrsHrs -23; this.endMin = parseInt(this.strtMin) + this.hrsMin - 60; }
+      else  { this.endHrs = parseInt(this.strtHrs) + this.hrsHrs + 1; this.endMin = parseInt(this.strtMin) + this.hrsMin - 60; }
+    }
+
+    else {
+      if (parseInt(this.strtHrs) + this.hrsHrs > 24)
+            { this.endHrs = parseInt(this.strtHrs) + this.hrsHrs -24; this.endMin = parseInt(this.strtMin) + this.hrsMin;      }
+      else  { this.endHrs = parseInt(this.strtHrs) + this.hrsHrs;     this.endMin = parseInt(this.strtMin) + this.hrsMin;      }
+    }
+
+    if( this.endHrs < 10 ) {
+      if( this.endHrs === 0 ) { this.endHrs = '00'       }
+      else { this.endHrs = '0' + this.endHrs.toString(); }
+    } else { this.endHrs = this.endHrs.toString();       }
+
+    if( this.endMin === 0 ) { this.endMin = '00';        }
+    if( this.hrsMin === 0 ) { this.hrsMin = '00';        }
+
+    this.timeEnds           = this.endHrs + ':' + this.endMin;
+    workOrderData.timeEnds  = this.endHrs + ':' + this.endMin;
+    workOrderData.repairHrs = this.hrsHrs + ':' + this.hrsMin;
+
+    console.log(this.strtHrs);
+    console.log(this.endHrs);
+
+    workOrderData.repairHrs = this.hrsHrs + ':' + this.hrsMin;
   }
 
 }
