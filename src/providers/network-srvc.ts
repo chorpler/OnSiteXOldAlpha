7462de950de65afa961f09033849b4c62c4b62a3
David S <chorpler@gmail.com>
2017-05-22 04:59:03 -0500

    Thank goodness Github didn't want for the file to rename
    Grumble

--- src/providers/network-srvc.ts
+++ src/providers/network-srvc.ts
@@ -16,50 +16,50 @@ export class NetworkStatus {
   public static isOnline: boolean = true;
   public static isWiFi  : boolean = true;
 
-constructor(private network: Network) {
-  window['netSrvc'] = this;
-}
-
-watchForDisconnect() {
-  let disconnect = this.network.onDisconnect().subscribe(() => {
-    Log.l('Network disconnected');
-    NetworkStatus.isOnline = false;
-    NetworkStatus.isWiFi = false;
-  });
-}
-
-isConnected() {
-  if(this.network.type == 'wifi') {
-    NetworkStatus.isOnline = true;
-    NetworkStatus.isWiFi = true;
-    return true;
-  } else {
-    return false;
+  constructor(private network: Network) {
+    window['netSrvc'] = this;
   }
-}
 
-// disconnectSubscription.unsubscribe();
-// watch network for a disconnect
+  watchForDisconnect() {
+    let disconnect = this.network.onDisconnect().subscribe(() => {
+      Log.l('Network disconnected');
+      NetworkStatus.isOnline = false;
+      NetworkStatus.isWiFi = false;
+    });
+  }
 
-// stop disconnect watch
+  isConnected() {
+    if(this.network.type == 'wifi') {
+      NetworkStatus.isOnline = true;
+      NetworkStatus.isWiFi = true;
+      return true;
+    } else {
+      return false;
+    }
+  }
 
-watchForConnection() {
-  // watch network for a connection
-  let connectSubscription = this.network.onConnect().subscribe(() => {
-    console.log('network connected!'); 
-    // We just got a connection but we need to wait briefly
-     // before we determine the connection type.  Might need to wait 
-    // prior to doing any api requests as well.
-    setTimeout(() => {
-      if (this.network.type === 'wifi') {
-        console.log('we got a wifi connection, woohoo!');
-        // // stop connect watch
-        // connectSubscription.unsubscribe();
-      }
-    }, 3000);
-  });
- 
-}
+  // disconnectSubscription.unsubscribe();
+  // watch network for a disconnect
+
+  // stop disconnect watch
+
+  watchForConnection() {
+    // watch network for a connection
+    let connectSubscription = this.network.onConnect().subscribe(() => {
+      console.log('network connected!'); 
+      // We just got a connection but we need to wait briefly
+       // before we determine the connection type.  Might need to wait 
+      // prior to doing any api requests as well.
+      setTimeout(() => {
+        if (this.network.type === 'wifi') {
+          console.log('we got a wifi connection, woohoo!');
+          // // stop connect watch
+          // connectSubscription.unsubscribe();
+        }
+      }, 3000);
+    });
+   
+  }
 
 
 }
