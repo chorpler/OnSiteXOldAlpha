c99fab9918dac605c658b76e8c53063873f1a333
David Sargeant <chorpler@gmail.com>
2017-06-21 17:29:23 -0500

    uyt

---
+++ ironic.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+clear
+
+echo "Removing www directory non-ironically..."
+rm -rf ./www
+
+echo "Taking iron supplements internally..."
+echo "Getting laundry ironing done normally..."
+echo "Pondering pronunciation of 'iron' vs. 'ironic' ..."
+echo "Listening to Alanis Morissette song..."
+echo "Mocking improper usage of the term 'ironic' in Alanis Morissette song..."
+echo "Writing, directing, producing, and starring in third-rate knock-off superhero movie 'Ironic Man' ..."
+echo "Starting ionic serve ironically..."
+
+ionic serve -w 'google chrome' --lab
